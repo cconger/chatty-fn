44b939bed18aad0d374474485f2fa92154e7e01f
Channing Conger <cconger@gmail.com>
2019-08-26 16:08:49 -0700

    Do some less contrived logging
    Signed-off-by: Channing Conger <cconger@gmail.com>

--- main.go
+++ main.go
@@ -3,48 +3,69 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"time"
 )
 
-var letters = []byte("abcdefghijklmnopqrstuvwxyz")
+type ReqLogEntry struct {
+	Method        string
+	URL           string
+	Proto         string
+	Host          string
+	RemoteAddr    string
+	ContentLength int64
+	Body          string
+	Headers       map[string][]string
+	ReceivedAt    time.Time
+}
 
-func randString(l int) string {
-	b := make([]byte, l, l)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+// FromRequest returns a ReqLogEntry describing the assocated http.Request
+func FromRequest(r *http.Request) *ReqLogEntry {
+	entry := &ReqLogEntry{
+		Method:        r.Method,
+		URL:           r.URL.String(),
+		Proto:         r.Proto,
+		Host:          r.Host,
+		RemoteAddr:    r.RemoteAddr,
+		ContentLength: r.ContentLength,
+		ReceivedAt:    time.Now(),
+		Headers:       r.Header,
 	}
-	return string(b)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		body = []byte{}
+	}
+	entry.Body = string(body)
+	return entry
 }
 
 func noisy200(w http.ResponseWriter, r *http.Request) {
-	lb := make(map[string]string)
-	for i := 0; i < 100; i++ {
-		lb[randString(5)] = randString(20)
-	}
-
-	message, err := json.Marshal(lb)
+	log.Println("Received request")
+	message, err := json.Marshal(FromRequest(r))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 	}
+	log.Println("Request:", string(message))
 
-	for i := 0; i < 3; i++ {
-		log.Print(string(message))
-	}
-	fmt.Fprintf(os.Stdout, "This is me writing to stdout on request!\n")
-	fmt.Fprintf(os.Stdout, string(message))
+	// Purposely not a new line on this one
+	fmt.Fprintf(os.Stdout, "This is me writing to stdout on request: ")
+	fmt.Fprintln(os.Stdout, string(message))
 
+	fmt.Println("I liked what I got... so I'm gonna 200 OK")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
 func main() {
+	// Do not include my own timestamps
+	log.SetFlags(0)
+
 	log.Print("Starting application server")
-	fmt.Fprintf(os.Stderr, "This is a warning to stderr\n")
+	fmt.Fprintf(os.Stdout, "This is a warning to stdout\n")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", noisy200)
 
